197318c6e780ff0d2d3429fbcdc8ab811055dc8a
manrooh228 <maksatsuper57915@gmail.com>
2025-12-05 02:09:33 +0500

    Moving and Idle Animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,23 +1,62 @@
+using System;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Player : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
 
-    private float moveSpeed = 3.5f;
+    [SerializeField]private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
     private float xInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        HandleInput();
+        MoveHorizontal();
+        HandleAnimations();
+        FlipAnimations();
+    }
+
+    private void FlipAnimations()
+    {
+        if (rb.linearVelocityX < 0)
+            transform.rotation = new Quaternion(0, 180, 0, 0);
+        if (rb.linearVelocityX > 0)
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+
+
+    }
+
+    private void HandleAnimations()
+    {
+        bool isMoving = rb.linearVelocityX != 0;
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void HandleInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void MoveHorizontal()
+    {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
     }
 
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+    }
 }
