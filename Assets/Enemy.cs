2b086eb01611f16289161e5e51ebdda4fe940411
manrooh228 <maksatsuper57915@gmail.com>
2025-12-07 02:45:01 +0500

    Enemy Demo 1

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeDamaged;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeDamaged + redColorDuration) 
+            if (sr.color != Color.white)
+                TurnWhite();
+
+    }
+
     public void TakeDamage ()
     {
-        Debug.Log(gameObject.name + " taked some damage");
+        sr.color = Color.red;
+        lastTimeDamaged = currentTimeInGame;
+
+        
+    }
+
+    private void TurnWhite ()
+    {
+        sr.color = Color.white;
     }
 }
